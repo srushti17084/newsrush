cf06c070ffbc6e8da67d8b9f50b497859e0baf06
Srushti Uphade <srushti.17084@sakec.ac.in>
2025-11-10 13:35:46 +0530

    Initial commit

--- newsrush-main/cipher.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-passwords = open("samplepasswords.txt").read().splitlines()
-with open("sample_hashes.txt", "w") as out:
-    for p in passwords:
-        md5 = hashlib.md5(p.encode()).hexdigest()
-        sha1 = hashlib.sha1(p.encode()).hexdigest()
-        sha256 = hashlib.sha256(p.encode()).hexdigest()
-        out.write(f"{p} MD5:{md5} SHA1:{sha1} SHA256:{sha256}\n")
